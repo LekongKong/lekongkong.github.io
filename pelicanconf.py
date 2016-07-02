452ee7a4984975c91d0348973ee4d39857ee72cb
jswh <jswh@jswh.me>
2016-07-02 19:32:36 +0800

    write article

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,3 +35,22 @@ DEFAULT_PAGINATION = 15
 #RELATIVE_URLS = True
 
 THEME='Flex'
+
+SITETITLE = 'Lkk Team Blog'
+SITESUBTITLE = ''
+SITEDESCRIPTION = 'Lkk Team members Thoughts and Writings'
+SITELOGO = 'http://s.baixing.net/lkk/favicon.png'
+BROWSER_COLOR = '#333'
+
+FAVICON = 'http://s.baixing.net/lkk/favicon.png'
+ROBOTS = 'index, follow'
+
+COPYRIGHT_YEAR = 2018
+CC_LICENSE = { 'name': 'Creative Commons Attribution-ShareAlike', 'version':'4.0', 'slug': 'by-sa' }
+
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/custom.css'},
+}
+CUSTOM_CSS = 'static/custom.css'
+
+MAIN_MENU = True
