4809d4bfa0ddd229a3655584a87a5839eb423e8a
jswh <jswh@jswh.me>
2016-07-02 18:26:14 +0800

    write article

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,6 +35,15 @@ DEFAULT_PAGINATION = 15
 #RELATIVE_URLS = True
 
 THEME='blue-penguin'
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
 MENU_INTERNAL_PAGES = (
     ('Tags', TAGS_URL, TAGS_SAVE_AS),
     ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
